045d08ea9f85be1802de057c19dfaa627ecccbe5
Tushar Goel <tushar.goel@nagarro.com>
2024-12-05 13:51:08 +0530

    New Repo Created

--- MyMicroserviceController.cs
+++ MyMicroserviceController.cs
@@ -1,12 +1,12 @@
-using BHF.MS.MyMicroservice.Models;
-using BHF.MS.MyMicroservice.Services;
+using BHF.MS.test5.Models;
+using BHF.MS.test5.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BHF.MS.MyMicroservice.Controllers
+namespace BHF.MS.test5.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class MyMicroserviceController(ILogger<MyMicroserviceController> logger, IExampleService service) : ControllerBase
+    public class test5Controller(ILogger<test5Controller> logger, IExampleService service) : ControllerBase
     {
         [HttpGet]
         [ResponseCache(VaryByQueryKeys = ["*"], Duration = 60)]
